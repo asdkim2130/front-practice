d5511508f9fb361cf504a3912a502cd1b87b0628
HYOJIN <hjhwang08@naver.com>
2025-02-05 17:50:55 +0900

    feat. 애니메이션효과 적용 중

--- src/app/assignments/like/page.tsx
+++ src/app/assignments/like/page.tsx
@@ -1,18 +1,48 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "@/components/header";
+import animatedLike from "../../../../public/like-icon.json";
+import Lottie from "lottie-react";
 
 function Like() {
+  const lottieRef = useRef<any>(null);
   const [isLike, setIsLiked] = useState(false);
 
   return (
     <div>
-      <button onClick={() => setIsLiked(!isLike)}>
+      <button
+        onClick={() => setIsLiked(!isLike)}
+        className="bg-neutral-300 hover:bg-neutral-400 active:bg-neutral-500"
+      >
         {isLike === false ? "좋아요" : "좋아요취소"}
+      </button>
+
+      <button onClick={handleClick}>
+        <Lottie
+          animationData={animatedLike}
+          loop={false}
+          autoplay={false}
+          style={{ width: 100, height: 100 }}
+          onComplete={() => setIsLiked(true)}
+        />
       </button>
     </div>
   );
+
+  function handleClick() {
+    if (lottieRef.current) {
+      lottieRef.current.paly();
+    }
+    if (lottieRef.current) {
+      if (isLike) {
+        lottieRef.current.stop();
+        setIsLiked(false);
+      } else {
+        lottieRef.current.play();
+      }
+    }
+  }
 }
 
 export default function Page() {

b05510155f63c5ed764c3cceb69abbd1e9363501
HYOJIN <hjhwang08@naver.com>
2025-02-06 11:01:58 +0900

    feat. id, password consol 출력

--- src/app/assignments/login/page.tsx
+++ src/app/assignments/login/page.tsx
@@ -1,12 +1,30 @@
 export default function LoginPage() {
+  async function handleSubmit(formData: FormData) {
+    "use server";
+
+    const rawFormData = {
+      id: formData.get("id"),
+      password: formData.get("password"),
+    };
+
+    console.log({ ...rawFormData });
+  }
+
   return (
     <div>
       <h1>로그인페이지</h1>
-      <form>
-        <label>아이디</label>
-        <input type="text" className="border border-black" />
-        <label>패스워드</label>
-        <input type="password" className="border border-black" />
+      <br></br>
+      <form action={handleSubmit}>
+        <label>아이디 </label>
+        <input type="text" name="id" className="border border-black" />
+        <br></br>
+        <label> 패스워드 </label>
+        <input
+          type="password"
+          name="password"
+          className="border border-black"
+        />
+        <br></br>
         <button>로그인</button>
       </form>
     </div>
